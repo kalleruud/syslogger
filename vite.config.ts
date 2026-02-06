dd1a25220b1d53f52d174239e31bfead641acfe6
kalleruud <ole@kallerud.no>
2026-02-06 21:18:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -8,7 +6,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-react-compiler'],
+        plugins: [['babel-plugin-react-compiler']],
       },
     }),
     tailwindcss(),
