22a34c9c93784d7d2e29cf0f078c4368bc307aba
kalleruud <ole@kallerud.no>
2026-02-06 21:18:43 +0100

    Revert "Update vite.config.ts"
    This reverts commit dd1a25220b1d53f52d174239e31bfead641acfe6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -6,7 +8,7 @@ export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: ['babel-plugin-react-compiler'],
       },
     }),
     tailwindcss(),
