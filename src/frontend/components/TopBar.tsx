2b68204d7e26dea139d2e15de93140d8d8e6176b
kalleruud <ole@kallerud.no>
2026-02-12 18:25:10 +0100

    Use logo in top bar

--- src/frontend/components/TopBar.tsx
+++ src/frontend/components/TopBar.tsx
@@ -1,9 +1,13 @@
+import logo from '@public/logo.svg'
 import ColumnSelector from './ColumnSelector'
 
 export default function TopBar() {
   return (
     <div className='absolute top-0 right-0 left-0 z-50 flex h-16 items-center justify-between border-b bg-background/50 px-4 backdrop-blur-lg'>
-      <h1 className='text-lg font-black'>syslogger</h1>
+      <div className='flex items-center gap-2'>
+        <img src={logo} alt='syslogger logo' className='h-6 w-6' />
+        <h1 className='text-lg font-black'>syslogger</h1>
+      </div>
       <ColumnSelector />
     </div>
   )
