7a203208ae3f1c3d7aec2c64568b475d2025974d
kalleruud <ole@kallerud.no>
2026-02-20 00:50:40 +0100

    adjust tests

--- src/backend/parsers/parser.spec.ts
+++ src/backend/parsers/parser.spec.ts
@@ -1,6 +1,10 @@
+import { describe, expect, test } from 'bun:test'
+
 import type { Log } from '@/database/schema'
 import { FACILITIES } from '@/lib/facilities'
 import { SEVERITIES } from '@/lib/severities'
+
+import parseSyslog from './parser'
 
 const testCases: {
   log: string
@@ -9,7 +13,7 @@ const testCases: {
   {
     log: '<30>Feb 20 00:19:00 media-seerr[15429]: 2026-02-19T23:19:00.002Z [debug][Jobs]: Starting scheduled job: Download Sync',
     expected: {
-      timestamp: '2026-02-20T00:19:00Z',
+      timestamp: '2026-02-19T23:19:00.002Z',
       severity: SEVERITIES.debug.level,
       facility: FACILITIES.daemons.level,
       hostname: null,
@@ -23,7 +27,7 @@ const testCases: {
   {
     log: '<13>Feb 20 00:00:01 Svetlana move: mover: started',
     expected: {
-      timestamp: '2026-02-20T00:00:01Z',
+      timestamp: '2026-02-19T23:00:01.000Z',
       severity: SEVERITIES.notice.level,
       facility: FACILITIES.user.level,
       hostname: 'Svetlana',
@@ -33,4 +37,31 @@ const testCases: {
       message: 'mover: started',
     },
   },
+  {
+    log: '<30>Feb 20 00:45:25 media-unpackerr[15429]: [INFO] 2026/02/20 00:45:25.976377 sonarr.go:74: [Sonarr] Updated (http://media.vpn.local:8989): 0 Items Queued, 0 Retrieved',
+    expected: {
+      timestamp: '2026-02-20T00:45:25.976Z',
+      severity: SEVERITIES.info.level,
+      facility: FACILITIES.daemons.level,
+      hostname: null,
+      appname: 'media-unpackerr',
+      procid: '15429',
+      msgid: null,
+      message:
+        '[INFO] 2026/02/20 00:45:25.976377 sonarr.go:74: [Sonarr] Updated (http://media.vpn.local:8989): 0 Items Queued, 0 Retrieved',
+    },
+  },
 ]
+
+describe('parseSyslog', () => {
+  for (const [index, testCase] of testCases.entries()) {
+    test(`parses syslog message #${index + 1}`, () => {
+      const result = parseSyslog(testCase.log)
+
+      expect(result.log).toEqual({
+        ...testCase.expected,
+        raw: testCase.log,
+      })
+    })
+  }
+})
